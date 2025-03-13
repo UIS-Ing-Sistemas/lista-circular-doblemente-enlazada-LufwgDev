d3d7658ddb6b6f41ebf9b5a88072800a1edb65c3
LufwgDev <lufwg@proton.me>
2025-03-13 15:14:44 -0500

    feat: :sparkles: delete, print and lenght methods added
    deleteElement, a specific element in case it is on the list, delete the firs founded, printElements gets a parameter, if True prints all elements on the list from head to tail, if False, form tail to head

--- base-CircularLinkedList.py
+++ base-CircularLinkedList.py
@@ -41,7 +41,7 @@ class CircularLinkedList:
 
 
 
-    def IsEmpty(self):
+    def isEmpty(self):
         if self.head == None:
             return True
         else:
@@ -60,4 +60,90 @@ class CircularLinkedList:
             
             self.tail = newNode
             return True
+
+    
+    def deleteElement(self,x):
+        #if (self.isValueOnList(x)):
+        currentTemporalNode = None
+        pastTemporalNode = None
+        escape = False
+        if self.isEmpty():
+            return False
+        else:
+            currentTemporalNode=self.head
+            pastTemporalNode=currentTemporalNode.prev
+
+            if currentTemporalNode.data == x:
+                self.head=currentTemporalNode.next
+            if pastTemporalNode.data == x:
+                self.tail=pastTemporalNode.prev
+
+            while escape !=True:
+
+                if currentTemporalNode.data == x:
+                    
+                    pastTemporalNode.next=currentTemporalNode.next
+                    futureTemporalNode=currentTemporalNode.next
+                    futureTemporalNode.prev=pastTemporalNode
+                    return True
+                else:
+                    if currentTemporalNode.next!=self.head:
+                        pastTemporalNode=currentTemporalNode
+                        currentTemporalNode = currentTemporalNode.next
+                    else:
+                        escape = True
+        return False
+
+    def printElements(self,order):
+        if self.isEmpty():
+            return False
+        else:
+            if order:
+                
+                temporalNode = self.head
+                head = self.head
+                i=0
+                while(temporalNode!=head or i==0):
+                    print(temporalNode.data)
+                    temporalNode=temporalNode.next
+                    i=i+1
+            else:
+                tail = self.tail
+                temporalNode= self.tail
+                i=0
+                while(temporalNode!=tail or i==0):
+                    print(temporalNode.data)
+                    temporalNode=temporalNode.prev
+                    i=i+1
+                
+    def lenght(self):
+        count = 0
+        if self.isEmpty():
+            return count
+        else:
+            temporalNode = self.head
+            head = self.head
+            while(temporalNode!=head or count==0):
+                count=count+1
+                temporalNode=temporalNode.next
+        return count
+
+
+#Tests
+mylist = CircularLinkedList()
+for i in range(10):
+    mylist.insertTail(3*i)
+
+#mylist.printElements()
+
+for i in range(1,7):
+    mylist.insertHead(7*i)
+
+mylist.printElements(True)
+print(f"hay {mylist.lenght()} elementos")
+mylist.deleteElement(18)
+mylist.deleteElement(42)
+print(f"hay {mylist.lenght()} elementos")
+mylist.printElements(True)
+mylist.printElements(False)
 
